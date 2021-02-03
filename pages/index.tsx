040e055b31ef3b2aa6b7690184b5e15f538f58bc
wpkarolharabasz <karol.harabasz@wpengine.com>
2021-02-03 10:43:54 +0100

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+const Home = () => <h1>Hello, Headless!</h1>;
+const https = require('https');
+let resppp;
+https.get('https://karolandcat1.wpengine.com/?rest_route=/wp/v2/posts', (resp) => {
+  let data = '';
+
+  // A chunk of data has been received.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(data);
+    resppp = data;
+
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+
+
+
+export default Home;
